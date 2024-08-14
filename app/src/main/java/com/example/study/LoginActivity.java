468f27d7a9227d2ef4a31949fb41d22f87f79f6c
GU-JQR <20210312213@squ.edu.cn>
2024-08-14 12:03:09 +0800

    登录页面

--- app/src/main/java/com/example/study/LoginActivity.java
+++ app/src/main/java/com/example/study/LoginActivity.java
@@ -2,13 +2,48 @@ package com.example.study;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText et_user,et_pass;
+    Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        et_user=findViewById(R.id.et_user);
+        et_pass=findViewById(R.id.et_pass);
+        btn_login=findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+
+                if(TextUtils.isEmpty(et_user.getText())){
+                    Toast.makeText(this,"用户名不能为空",Toast.LENGTH_LONG).show();
+                }
+                if(TextUtils.isEmpty(et_pass.getText())){
+                    Toast.makeText(this,"密码不能为空",Toast.LENGTH_LONG).show();
+                }
+                if(et_user.getText().toString().equals("tom")&&et_pass.getText().toString().equals("123456")){
+                    Intent intent = new Intent();
+                    intent.setClass(this,FrameActivity.class);
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(this,"登录失败",Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 }
