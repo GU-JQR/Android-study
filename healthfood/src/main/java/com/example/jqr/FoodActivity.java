1734eae533a65342ac1d61c5088dcd332a2a93d7
GU-JQR <20210312213@squ.edu.cn>
2024-08-20 11:55:36 +0800

    美食列表

--- healthfood/src/main/java/com/example/jqr/FoodActivity.java
+++ healthfood/src/main/java/com/example/jqr/FoodActivity.java
@@ -3,21 +3,26 @@ package com.example.jqr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.jqr.adapter.MyAdapter;
 import com.example.jqr.entiyt.Food;
+import com.example.jqr.utils.DBHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FoodActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private DBHelper dbHelper;
     //声明控件
-    ImageView iv_back;
+    ImageView iv_back, iv_search_btn, iv_flush;
+    EditText et_search;
     private List<Food> myDatas;
     //声明适配器
     private MyAdapter myAdapter;
@@ -28,19 +33,26 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+        //初始化数据源
+        initData();
         //获取控件
         iv_back = findViewById(R.id.iv_back);
         lv = findViewById(R.id.lv);
+        et_search = findViewById(R.id.et_search);
+        iv_search_btn = findViewById(R.id.iv_search_btn);
+        iv_flush = findViewById(R.id.iv_flush);
         //绑定点击监听
         iv_back.setOnClickListener(this);
+        iv_search_btn.setOnClickListener(this);
+        iv_flush.setOnClickListener(this);
 
         //ListView相关  创建数据源
-        myDatas = new ArrayList<>();
-        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
-        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
-        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
-        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
-        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
+//        myDatas = new ArrayList<>();
+//        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
+//        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
+//        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
+//        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
+//        myDatas.add(new Food(1, "猪肉", "黄莲", "R.mipmap.pork", "好吃"));
 
         //创建适配器
         myAdapter = new MyAdapter(myDatas, this);
@@ -57,6 +69,62 @@ public class FoodActivity extends AppCompatActivity implements View.OnClickListe
                 //返回
                 finish();
                 break;
+            case R.id.iv_search_btn:
+                //根据条件搜索想要的数据信息
+                seatchData();
+                break;
+            case R.id.iv_flush:
+                flushData();
+                break;
+        }
+    }
+
+    //刷新数据
+    private void flushData() {
+        //1.获取数据库中查询的全部信息
+        List<Food> foodList = dbHelper.getAllFood();
+        //清空数据
+        myDatas.clear();
+        //2.传递给数据源
+        for (Food food : foodList) {
+            myDatas.add(food);
+        }
+        //3.将搜索框文本清空
+        et_search.setText("");
+        //通知适配器更新
+        myAdapter.notifyDataSetChanged();
+
+    }
+
+    //搜索信息
+    private void seatchData() {
+        //1.从编辑框中获取输入的信息，判断是否为空
+        if (TextUtils.isEmpty(et_search.getText().toString())) {
+            Toast.makeText(this, "搜索内容不能为空", Toast.LENGTH_SHORT).show();
+            return;//退出方法  不往下执行
+        }
+
+        //2. 根据填写的key搜索信息
+        List<Food> foodList = dbHelper.getFoodByName(et_search.getText().toString());
+        //3. 将这个集合数据传递给数据源
+        myDatas.clear();
+        for (Food food : foodList) {
+            myDatas.add(food);
+        }
+        //4.更新适配器
+        myAdapter.notifyDataSetChanged();
+    }
+
+    public void initData() {
+        //实例化dbhelper
+        dbHelper = DBHelper.getInstance(this);
+        //通过dbhelper中查询全部信息getAllFood方法，获取数据源
+        List<Food> foodList = dbHelper.getAllFood();
+        //将查询结果传递给数据源
+        myDatas = new ArrayList<>();
+        for (int i = 0; i < foodList.size(); i++) {
+            myDatas.add(foodList.get(i));
+
         }
     }
 }
--- healthfood/src/main/java/com/example/jqr/utils/DBHelper.java
+++ healthfood/src/main/java/com/example/jqr/utils/DBHelper.java
@@ -1,12 +1,19 @@
 package com.example.jqr.utils;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
+
+import com.example.jqr.entiyt.Food;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
     //声明静态常亮  静态常量
@@ -75,5 +82,60 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.d("JQR", "数据库更新...");
     }
+
+    //查询全部食品信息
+    @SuppressLint("Range")
+    public List<Food> getAllFood() {
+        List<Food> foodList = new ArrayList<>();
+        //1.获取数据库查询操作对象（读操作）已经创建了mRDB 可以直接使用
+        //2.利用mRDB查询
+        Cursor cursor = mRDB.query(TABLE_NAME, null, null, null, null, null, null);
+        //3.通过游标遍历查询的结构
+        while (cursor.moveToNext()) {
+            //4.构建每一条数据库中记录的实体对象food
+            int _id = cursor.getInt(cursor.getColumnIndex(cursor.getColumnName(0)));
+            String foodname = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(1)));
+            String noeatfood = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(2)));
+            String picpath = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(3)));
+            String foodesc = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(4)));
+            //测试
+            Log.d("JQR", _id + " " + foodname + " " + noeatfood + " " + picpath);
+            //创建Food
+            Food food = new Food(_id, foodname, noeatfood, picpath, foodesc);
+            //5.将构建的实体对象Food添加到foodList集合中
+            foodList.add(food);
+        }
+        //6.返回集合foodList
+        return foodList;
+    }
+
+    //    根据食品名称关键字搜索对应的食品信息  例如：猪->相关猪的食品
+//    sql: select * from table_food where foodname like '%猪%';
+    @SuppressLint("Range")
+    public List<Food> getFoodByName(String key) {
+        //声明一个集合用了存放查询的数据
+        List<Food> foodList = new ArrayList<>();
+        //1.读操作 使用mRDB进行操作
+//        Cursor cursor = mRDB.query(TABLE_NAME, new String[]{"_id", "foodname", "noeatfood", "picpath", "foodesc"}, "foodname like '%?%'", new String[]{key}, null, null, null);
+        Cursor cursor = mRDB.rawQuery("select * from table_food where foodname like ?", new String[]{"%" + key + "%"});
+        //2.通过查询得到游标遍历结果
+        while (cursor.moveToNext()) {
+            //4.构建每一条数据库中记录的实体对象food
+            int _id = cursor.getInt(cursor.getColumnIndex(cursor.getColumnName(0)));
+            String foodname = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(1)));
+            String noeatfood = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(2)));
+            String picpath = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(3)));
+            String foodesc = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(4)));
+            //测试
+            Log.d("JQR", _id + " " + foodname + " " + noeatfood + " " + picpath);
+            //创建Food
+            Food food = new Food(_id, foodname, noeatfood, picpath, foodesc);
+            //5.将构建的实体对象Food添加到foodList集合中
+            foodList.add(food);
+        }
+        //返回查询的结果集合
+        return foodList;
+    }
+    //添加食品信息
 
 }
